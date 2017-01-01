d03aad25d1a7113b399690a3a07e13a3d0b3f54d
Chris Thielen <github@sandgnat.com>
2017-01-01 14:54:38 -0600

    publishing 1.0.7

--- publish.js
+++ publish.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
 var pkg = require('./package.json');
-var ver = pkg.version;
+let ver = pkg.version;
 
-console.log("Run this command to publish:\n\n")
-console.log("git tag " + ver + " && git push origin " + ver + " && npm publish\n\n\n");
+console.log("Run these commands to publish:\n\n")
+console.log("npm run build && git status\n\n");
+console.log("git commit -m 'publishing " + ver + "' . && git push\n\n");
+console.log("git tag " + ver + " && git push origin " + ver + " && npm publish\n\n");
