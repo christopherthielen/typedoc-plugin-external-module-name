855b87dbc0ecc9ae4c4ef383b0c360b9028cda39
Chris Thielen <github@sandgnat.com>
2017-01-01 14:11:57 -0600

    chmod +x publish.js

--- publish.js
+++ publish.js
@@ -1,7 +1,7 @@
-#!node
+#!/usr/bin/env node
 
 var pkg = require('./package.json');
-let ver = pkg.version;
+var ver = pkg.version;
 
 console.log("Run this command to publish:\n\n")
 console.log("git tag " + ver + " && git push origin " + ver + " && npm publish\n\n\n");
