db4232d56c1fa134960e81cad1260cfca3a9b8ee
Chris Thielen <github@sandgnat.com>
2017-01-17 09:12:09 -0600

    fix(regexp): Fix the module match regexp. chore(*): Add build step to package prepublish chore(*): lint publish.js

--- publish.js
+++ publish.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
 var pkg = require('./package.json');
-let ver = pkg.version;
+var ver = pkg.version;
 
-console.log("Run these commands to publish:\n\n")
+console.log("Run these commands to publish:\n\n");
 console.log("npm run build && git status\n\n");
 console.log("git commit -m 'publishing " + ver + "' . && git push\n\n");
 console.log("git tag " + ver + " && git push origin " + ver + " && npm publish\n\n");
