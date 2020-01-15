35dcf5ef89d7c408c1b4a34d34fdcfdf09a888a5
Chris Thielen <cthielen@netflix.com>
2020-01-14 17:13:53 -0800

    test(typedoc-0.14): Add a test for parent.child typedoc modules

--- test/typedoc-latest/src/index.ts
+++ test/typedoc-latest/src/index.ts
@@ -1,8 +1,9 @@
 /** @module root */
 
-export * from './file1';
-export * from './file2';
-export * from './dir1';
-export * from './dir2';
+import './file1';
+import './file2';
+import './dir1';
+import './dir2';
+import './parentchild';
 import * as dir1 from './dir1';
 export const reexport_dir1 = dir1;
